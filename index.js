61c10e3dd4d3bad3b30795a3aed79065c8450e67
Julia Popp <juliampopp@gmail.com>
2022-11-11 19:28:50 -0800

    commit tree 426047c081c313b8af898bdb6f9bc1f28d38cd77

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const friends = []
+        for(let i = 0; i < names.length; i++) {
+            friends.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+        return friends
+    }
+
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
